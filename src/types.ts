698ef25279d8891f5896baa2fe422251e02fefc1
Simon Baars <simon.mailadres@gmail.com>
2023-10-31 11:35:34 +0000

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,32 @@
+type Pair = {
+  first: string;
+  second: number;
+};
+
+interface Tuple2 {
+  first: string;
+  second: number;
+}
+
+class Dyad {
+  constructor(
+    public first: string,
+    public second: number,
+  ) {}
+}
+
+function test1(input: Pair) {
+  console.log(`test1 called with ${input.first} and ${input.second}`);
+}
+
+function test2(input: Tuple2) {
+  console.log(`test2 called with ${input.first} and ${input.second}`);
+}
+
+function test3(input: Dyad) {
+  console.log(`test3 called with ${input.first} and ${input.second}`);
+}
+
+test1({ first: "a", second: 1 });
+test2({ first: "b", second: 2 });
+test3(new Dyad("c", 3));
