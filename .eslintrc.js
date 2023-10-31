572e2a719ae659ddec923b76715501c375d777d6
Simon Baars <simon.mailadres@gmail.com>
2023-10-31 11:35:24 +0000

    Create Typescript project structure

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "google",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/lib/**/*", // Ignore built files.
+    "jest.config.js",
+  ],
+  plugins: ["@typescript-eslint", "import", "prettier"],
+  rules: {
+    quotes: ["error", "double"],
+    "import/no-unresolved": 0,
+    indent: "off",
+    // "max-len": ["error", {"code": 2000}],
+    "require-jsdoc": 0,
+    "linebreak-style": ["error", "unix"],
+    semi: ["error", "always"],
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/no-non-null-assertion": 0,
+    "prettier/prettier": "error",
+  },
+};
